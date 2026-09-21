a90e34bc60cdcff5aed7f4aa78e6900683112228
guayocsgo <vidoretpedro@gmail.com>
2026-09-21 16:38:22 -0300

    UGS terminado

--- Assets/Scripts/PanelHistorial.cs
+++ Assets/Scripts/PanelHistorial.cs
@@ -1,11 +1,15 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 using System.Text;
 
 public class PanelHistorial : MonoBehaviour
 {
-    public GameObject panel; // el panel completo que se muestra/oculta
+    public GameObject panel;
     public TMP_Text textoHistorial;
+    public RectTransform content; 
+    public ScrollRect scrollRect; 
 
     public async void MostrarHistorial()
     {
@@ -17,21 +21,28 @@ public class PanelHistorial : MonoBehaviour
         if (historial.disparos.Count == 0)
         {
             textoHistorial.text = "Todavía no hay disparos guardados.";
-            return;
         }
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < historial.disparos.Count; i++)
+        else
         {
-            DisparoResultado d = historial.disparos[i];
-            sb.AppendLine($"<b>Disparo {i + 1}</b> - {d.fecha}");
-            sb.AppendLine($"Ángulo: {d.angulo:F0}° | Horizontal: {d.anguloHorizontal:F0}°");
-            sb.AppendLine($"Fuerza: {d.fuerza:F1} | Masa: {d.masaProyectil:F1}");
-            sb.AppendLine($"{(d.acierto ? "✔ Acierto" : "✘ Falló")} | Distancia: {d.distancia:F1}m | Objetos afectados: {d.objetosAfectados}");
-            sb.AppendLine("――――――――――");
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < historial.disparos.Count; i++)
+            {
+                DisparoResultado d = historial.disparos[i];
+                sb.AppendLine($"<b>Disparo {i + 1}</b> - {d.fecha}");
+                sb.AppendLine($"Ángulo: {d.angulo:F0}° | Horizontal: {d.anguloHorizontal:F0}°");
+                sb.AppendLine($"Fuerza: {d.fuerza:F1} | Masa: {d.masaProyectil:F1}");
+                sb.AppendLine($"{(d.acierto ? "✔ Acierto" : "✘ Falló")} | Distancia: {d.distancia:F1}m | Objetos afectados: {d.objetosAfectados}");
+                sb.AppendLine("――――――――――");
+            }
+            textoHistorial.text = sb.ToString();
         }
 
-        textoHistorial.text = sb.ToString();
+        
+        Canvas.ForceUpdateCanvases();
+        content.sizeDelta = new Vector2(content.sizeDelta.x, textoHistorial.preferredHeight + 20f);
+
+        
+        scrollRect.verticalNormalizedPosition = 1f;
     }
 
     public void CerrarPanel()
